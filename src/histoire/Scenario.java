f0c447b868ed20c8915161aa2ed55729bbd11f0c
valerioangione <valerio.angione@hotmail.fr>
2023-09-30 16:22:00 +0200

    test ordi portable

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -8,7 +8,7 @@ import villagegaulois.Village;
 public class Scenario {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("helloooo");
 		Village village = new Village("le village des irréductibles", 10, 5);
 		Chef abraracourcix = new Chef("Abraracourcix", 10, village);
 		village.setChef(abraracourcix);
